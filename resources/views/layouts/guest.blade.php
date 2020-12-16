8fda65388f9395dfeee8cb49375ae48ba389f96f
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-12-16 02:48:27 -0500

    Conoce Livewire y aprende a usarlo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        @livewireStyles
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
@@ -20,5 +21,6 @@
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
+        @livewireScripts
     </body>
 </html>
