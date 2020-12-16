3a8b949915692689360babf9dd043efb16db950b
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-12-16 00:10:16 -0500

    Aplica Tailwind CSS para terminar el Landing Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,42 @@
                 @endauth
             </div>
         </nav>
-    </div>
-    <div class="flex flex-col bg-pink-500 h-screen">
+        <div class="flex container mx-auto items-center h-full">
+            <div class="flex w-1/3 flex-col items-start">
+                <h1 class="font-bold text-white text-5xl leading-tight mb-4">
+                    Simple generic landing page to subscribe
+                </h1>
 
+                <p class="text-indigo-200 text-xl mb-10">
+                    We are just checking the <span class="font-bold underline">TALL</span> stack. Would you mind subscribing?
+                </p>
+
+                <x-button class="py-3 px-8 bg-red-500 hover:bg-red-600">
+                    Subscribe
+                </x-button>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col w-full bg-pink-500 h-screen">
+        <p class="text-white font-extrabold text-5xl text-center">
+            Let's do it!
+        </p>
+
+        <form class="flex flex-col items-center p-24">
+            <x-input
+                class="px-5 py-3 w-80 border border-blue-400"
+                type="email"
+                name="email"
+                placeholder="Email address"
+            ></x-input>
+            <span class="text-gray-100 text-xs">
+                We will send you a confirmation email.
+            </span>
+            <x-button
+                class="px-5 py-3 mt-5 w-80 bg-blue-500 justify-center"
+            >
+                Get In
+            </x-button>
+        </form>
     </div>
 </x-guest-layout>
