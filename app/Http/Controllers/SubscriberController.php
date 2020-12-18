f725c065939dd762e3c1347bc5dc50118d000c31
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2020-12-18 01:01:42 -0500

    Implementa los componentes de Laravel para mostrar la lista de suscriptores

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 
 class SubscriberController extends Controller
 {
+    public function all()
+    {
+        return view('subscribers.all')->with([
+            'subscribers' => Subscriber::all(),
+        ]);
+    }
+
     public function verify(Subscriber $subscriber)
     {
         if (! $subscriber->hasVerifiedEmail()) {
--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\SubscriberController;
+
 Route::get('/', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('subscriber', [SubscriberController::class, 'all'])
+    ->name('subscribers.all');
